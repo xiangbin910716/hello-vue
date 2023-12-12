32448d421570987ada71adf47d1f4bd209d88886
xiangbin <Xb910716>
2023-12-12 09:54:31 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import App from "./App";
 import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import axios from "axios";
+import qs from "qs";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
 
 Vue.use(router);
 Vue.use(ElementUI);
--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -10,7 +10,30 @@
 <script>
 export default {
   props: ["id"],
-  name: "Profile"
+  name: "Profile",
+  //过滤器，
+  beforeRouteEnter: (to, from, next) => {
+    console.log("进入路由之前"); //加载数据
+    next(vm => {
+      vm.getData(); //进入路由之前执行getData方法
+    });
+  },
+  beforeRouteLeave: (to, from, next) => {
+    console.log("准备离开个人信息页");
+    next();
+  },
+  //axios
+  methods: {
+    getData: function() {
+      this.axios({
+        method: "get",
+        headers: { "Content-Type": "application/json" },
+        url: "http://127.0.0.1:8080/static/mock/data.json"
+      }).then(function(response) {
+        console.log(response);
+      });
+    }
+  }
 };
 </script>
 
